081fad2109fe645f9c46a34156793ae5b11eab74
LiC3MeR <ybaticlasher@gmail.com>
2024-05-28 20:32:57 +0300

    url_for

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, redirect, flash
+from flask import Flask, render_template, request, jsonify, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
