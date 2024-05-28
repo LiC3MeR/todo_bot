1bfb4616faec7bda224086f65805ef475831eb05
LiC3MeR <ybaticlasher@gmail.com>
2024-05-28 20:27:25 +0300

    flash

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, redirect
+from flask import Flask, render_template, request, jsonify, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
