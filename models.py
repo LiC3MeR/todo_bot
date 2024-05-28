2e3183f0a91ae259e6a13b227bd653b1805a8dad
LiC3MeR <ybaticlasher@gmail.com>
2024-05-28 20:44:47 +0300

    UserMixin

--- models.py
+++ models.py
@@ -11,5 +11,17 @@ class User(db.Model, UserMixin):
     def is_active(self):
         return True
 
+    class Task(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        task_id = db.Column(db.String(80), unique=True, nullable=False)
+        content = db.Column(db.String(200), nullable=False)
+        priority = db.Column(db.Integer, nullable=False)
+        description = db.Column(db.String(500), nullable=False)
+        project_id = db.Column(db.Integer, nullable=False)
+        status = db.Column(db.Integer, nullable=False)
+
+        def __repr__(self):
+            return f'<Task {self.content}>'
+
     def __repr__(self):
         return f"<User {self.username}>"
