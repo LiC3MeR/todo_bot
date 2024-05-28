2b828bb8049dd198c9135f0265d26bab5f0e483a
LiC3MeR <ybaticlasher@gmail.com>
2024-05-28 20:41:48 +0300

    UserMixin

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify, redirect, flash, url
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from flask_login import LoginManager, login_required, login_user, logout_user, current_user
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import User, Task
 from flask_bcrypt import generate_password_hash
